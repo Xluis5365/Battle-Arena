f89b7481c91e2b59866d5ce5086f9ca19bb5966e
Luis melendez <Xluisprox5365@gmail.com>
2021-09-07 18:25:02 -0400

    tried adding an auto respawn for enemies

--- Battle arena/Enemy2.gd
+++ Battle arena/Enemy2.gd
@@ -1,8 +1,36 @@
 extends KinematicBody
 
+onready var respawn = $respawn
 
 var health = 100
+
+func _ready():
+	pass
 
 func _process(delta):
 	if health <= 0:
 		queue_free()
+
+func _on_respawn_timeout():
+	print("im back bitches")
+
+
+func _on_Enemy2_tree_exited():
+	respawn.start()
+
+
+func _on_Enemy3_tree_exited():
+	print("dead")
+
+
+func _on_Enemy4_tree_exited():
+	print("dead")
+
+func _on_Enemy5_tree_exited():
+	print("dead")
+
+func _on_Enemy6_tree_exited():
+	print("dead")
+
+
+
